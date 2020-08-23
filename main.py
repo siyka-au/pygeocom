0f3ea953c5f09c6e2a5f151ad1aef9bdb6c438a7
Michael A <malleblas@siyka.com>
2020-08-23 21:51:42 +1000

    Motorisation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import serial
-from pygeocom import PyGeoCom, RecordFormat
+from pygeocom import PyGeoCom, RecordFormat, ControllerMode, ControllerStopMode, LockInStatus
 from datetime import datetime
+from time import sleep, time
 
 def main():
     ser = serial.Serial('/dev/ttyUSB0', 19200, timeout=1)
@@ -39,6 +40,34 @@ def main():
     a = geo.get_record_format()
     print(a)
     #geo.set_record_format(RecordFormat.GSI_8 if a == RecordFormat.GSI_16 else RecordFormat.GSI_16)
+
+    #geo.laser_pointer_on()
+    #sleep(1)
+    #geo.laser_pointer_off()
+    #sleep(1)
+    #geo.laser_pointer_on()
+    #sleep(1)
+    #geo.laser_pointer_off()
+
+    #print(geo.get_egl_intensity())
+
+    print(geo.get_motor_lock_status())
+
+
+    geo.start_controller(ControllerMode.LOCK_IN)
+
+    timeout = 5   # [seconds]
+    timeout_start = time()
+
+    while time() < timeout_start + timeout:
+        if geo.get_motor_lock_status() == LockInStatus.LOCKED_IN:
+            break
+
+    print(geo.get_motor_lock_status())
+
+    geo.stop_controller(ControllerStopMode.NORMAL)
+
+    print(geo.get_motor_lock_status())
 
     ser.close()
 
--- pygeocom.py
+++ pygeocom.py
@@ -2,6 +2,236 @@ from typing import Tuple, Any
 from enum import Enum, IntFlag
 from datetime import datetime
 
+GRC_TPS = 0x0000   # main return codes (identical to RC_SUP!!)
+GRC_SUP = 0x0000   # supervisor task (identical to RCBETA!!)
+GRC_ANG = 0x0100   # angle- and inclination
+GRC_ATA = 0x0200   # automatic target acquisition
+GRC_EDM = 0x0300   # electronic distance meter
+GRC_GMF = 0x0400   # geodesy mathematics & formulas
+GRC_TMC = 0x0500   # measurement & calc
+GRC_MEM = 0x0600   # memory management
+GRC_MOT = 0x0700   # motorization
+GRC_LDR = 0x0800   # program loader
+GRC_BMM = 0x0900   # basics of man machine interface
+GRC_TXT = 0x0A00   # text management
+GRC_MMI = 0x0B00   # man machine interface
+GRC_COM = 0x0C00   # communication
+GRC_DBM = 0x0D00   # data base management
+GRC_DEL = 0x0E00   # dynamic event logging
+GRC_FIL = 0x0F00   # file system
+GRC_CSV = 0x1000   # central services
+GRC_CTL = 0x1100   # controlling task
+GRC_STP = 0x1200   # start + stop task
+GRC_DPL = 0x1300   # data pool
+GRC_WIR = 0x1400   # wi registration
+GRC_USR = 0x2000   # user task
+GRC_ALT = 0x2100   # alternate user task
+GRC_AUT = 0x2200   # automatization
+GRC_AUS = 0x2300   # alternative user
+GRC_BAP = 0x2400   # basic applications
+GRC_SAP = 0x2500   # system applications
+GRC_COD = 0x2600   # standard code function
+GRC_BAS = 0x2700   # GeoBasic interpreter
+GRC_IOS = 0x2800   # Input-/ output- system
+GRC_CNF = 0x2900   # configuration facilities
+GRC_XIT = 0x2E00   # XIT subsystem (Excite-Level LIS)
+GRC_DNA = 0x2F00   # DNA2 subsystem
+GRC_ICD = 0x3000   # cal data management
+GRC_KDM = 0x3100   # keyboard display module
+GRC_LOD = 0x3200   # firmware loader
+GRC_FTR = 0x3300   # file transfer
+GRC_VNF = 0x3F00   # reserved for new TPS1200 subsystem
+GRC_GPS = 0x4000   # GPS subsystem
+GRC_TST = 0x4100   # Test subsystem
+GRC_PTF = 0x4F00   # reserved for new GPS1200 subsystem
+GRC_APP = 0x5000   # offset for all applications
+GRC_RES = 0x7000   # reserved code range 
+
+class ReturnCode(Enum):
+    GRC_OK              = GRC_TPS +  0   # Function successfully completed.
+    GRC_UNDEFINED       = GRC_TPS +  1   # Unknown error result unspecified.
+    GRC_IVPARAM         = GRC_TPS +  2   # Invalid parameter detected.\nResult unspecified.
+    GRC_IVRESULT        = GRC_TPS +  3   # Invalid result.
+    GRC_FATAL           = GRC_TPS +  4   # Fatal error.
+    GRC_NOT_IMPL        = GRC_TPS +  5   # Not implemented yet.
+    GRC_TIME_OUT        = GRC_TPS +  6   # Function execution timed out.\nResult unspecified.
+    GRC_SET_INCOMPL     = GRC_TPS +  7   # Parameter setup for subsystem is incomplete.
+    GRC_ABORT           = GRC_TPS +  8   # Function execution has been aborted.
+    GRC_NOMEMORY        = GRC_TPS +  9   # Fatal error - not enough memory.
+    GRC_NOTINIT         = GRC_TPS + 10   # Fatal error - subsystem not initialized.
+    GRC_SHUT_DOWN       = GRC_TPS + 12   # Subsystem is down.
+    GRC_SYSBUSY         = GRC_TPS + 13   # System busy/already in use of another process.\nCannot execute function.
+    GRC_HWFAILURE       = GRC_TPS + 14   # Fatal error - hardware failure.
+    GRC_ABORT_APPL      = GRC_TPS + 15   # Execution of application has been aborted (SHIFT-ESC).
+    GRC_LOW_POWER       = GRC_TPS + 16   # Operation aborted - insufficient power supply level.
+    GRC_IVVERSION       = GRC_TPS + 17   # Invalid version of file ...
+    GRC_BATT_EMPTY      = GRC_TPS + 18   # Battery empty
+    GRC_NO_EVENT        = GRC_TPS + 20   # no event pending.
+    GRC_OUT_OF_TEMP     = GRC_TPS + 21   # out of temperature range
+    GRC_INSTRUMENT_TILT = GRC_TPS + 22   # intrument tilting out of range
+    GRC_COM_SETTING     = GRC_TPS + 23   # communication error
+    GRC_NO_ACTION       = GRC_TPS + 24   # GRC_TYPE Input 'do no action'
+    GRC_SLEEP_MODE      = GRC_TPS + 25   # Instr. run into the sleep mode
+    GRC_NOTOK           = GRC_TPS + 26   # Function not successfully completed. 
+    GRC_NA              = GRC_TPS + 27   # Not available 
+    GRC_OVERFLOW        = GRC_TPS + 28   # Overflow error
+    GRC_STOPPED         = GRC_TPS + 29   # System or subsystem has been stopped
+    
+    GRC_COM_ERO                    = GRC_COM +  0  #  Initiate Extended Runtime Operation (ERO).
+    GRC_COM_CANT_ENCODE            = GRC_COM +  1  #  Cannot encode arguments in client.
+    GRC_COM_CANT_DECODE            = GRC_COM +  2  #  Cannot decode results in client.
+    GRC_COM_CANT_SEND              = GRC_COM +  3  #  Hardware error while sending.
+    GRC_COM_CANT_RECV              = GRC_COM +  4  #  Hardware error while receiving.
+    GRC_COM_TIMEDOUT               = GRC_COM +  5  #  Request timed out.
+    GRC_COM_WRONG_FORMAT           = GRC_COM +  6  #  Packet format error.
+    GRC_COM_VER_MISMATCH           = GRC_COM +  7  #  Version mismatch between client and server.
+    GRC_COM_CANT_DECODE_REQ        = GRC_COM +  8  #  Cannot decode arguments in server.
+    GRC_COM_PROC_UNAVAIL           = GRC_COM +  9  #  Unknown RPC, procedure ID invalid.
+    GRC_COM_CANT_ENCODE_REP        = GRC_COM + 10  #  Cannot encode results in server.
+    GRC_COM_SYSTEM_ERR             = GRC_COM + 11  #  Unspecified generic system error.
+    GRC_COM_UNKNOWN_HOST           = GRC_COM + 12  #  (Unused error code)
+    GRC_COM_FAILED                 = GRC_COM + 13  #  Unspecified error.
+    GRC_COM_NO_BINARY              = GRC_COM + 14  #  Binary protocol not available.
+    GRC_COM_INTR                   = GRC_COM + 15  #  Call interrupted.
+    GRC_COM_UNKNOWN_ADDR           = GRC_COM + 16  #  (Unused error code)
+    GRC_COM_NO_BROADCAST           = GRC_COM + 17  #  (Unused error code)
+    GRC_COM_REQUIRES_8DBITS        = GRC_COM + 18  #  Protocol needs 8bit encoded chararacters.
+    GRC_COM_UD_ERROR               = GRC_COM + 19  #  (Unused error code)
+    GRC_COM_LOST_REQ               = GRC_COM + 20  #  (Unused error code)
+    GRC_COM_TR_ID_MISMATCH         = GRC_COM + 21  #  Transacation ID mismatch error.
+    GRC_COM_NOT_GEOCOM             = GRC_COM + 22  #  Protocol not recognizeable.
+    GRC_COM_UNKNOWN_PORT           = GRC_COM + 23  #  (WIN) Invalid port address.
+    GRC_COM_ILLEGAL_TRPT_SELECTOR  = GRC_COM + 24  #  (Unused error code)
+    GRC_COM_TRPT_SELECTOR_IN_USE   = GRC_COM + 25  #  (Unused error code)
+    GRC_COM_INACTIVE_TRPT_SELECTOR = GRC_COM + 26 #  (Unused error code)
+    GRC_COM_ERO_END                = GRC_COM + 27  #  ERO is terminating.
+    GRC_COM_OVERRUN                = GRC_COM + 28  #  Internal error: data buffer overflow.
+    GRC_COM_SRVR_RX_CHECKSUM_ERROR = GRC_COM + 29  #  Invalid checksum on server side received.
+    GRC_COM_CLNT_RX_CHECKSUM_ERROR = GRC_COM + 30  #  Invalid checksum on client side received.
+    GRC_COM_PORT_NOT_AVAILABLE     = GRC_COM + 31  #  (WIN) Port not available.
+    GRC_COM_PORT_NOT_OPEN          = GRC_COM + 32  #  (WIN) Port not opened.
+    GRC_COM_NO_PARTNER             = GRC_COM + 33  #  (WIN) Unable to find TPS.
+    GRC_COM_ERO_NOT_STARTED        = GRC_COM + 34  #  Extended Runtime Operation could not be started.
+    GRC_COM_CONS_REQ               = GRC_COM + 35  #  Att to send cons reqs
+    GRC_COM_SRVR_IS_SLEEPING       = GRC_COM + 36 	#  TPS has gone to sleep. Wait and try again.
+    GRC_COM_SRVR_IS_OFF            = GRC_COM + 37  #  TPS has shut down. Wait and try again.
+    
+    GRC_EDM_SYSTEM_ERR         = GRC_EDM + 1 # Fatal EDM sensor error. See for the exact reason the original EDM sensor error number. In the most cases a service problem.
+    # Sensor user errors
+    GRC_EDM_INVALID_COMMAND    = GRC_EDM + 2 # Invalid command or unknown command, see command syntax.
+    GRC_EDM_BOOM_ERR           = GRC_EDM + 3 # Boomerang error.
+    GRC_EDM_SIGN_LOW_ERR       = GRC_EDM + 4 # Received signal to low, prisma to far away, or natural barrier, bad environment, etc.  
+    GRC_EDM_DIL_ERR            = GRC_EDM + 5 # Obsolete
+    GRC_EDM_SIGN_HIGH_ERR      = GRC_EDM + 6 # Received signal to strong, prism too near, stranger light effect.
+    # New TPS1200 sensor user errors
+    GRC_EDM_TIMEOUT            = GRC_EDM + 7 # Timeout, measuring time exceeded (signal too weak, beam interrupted,..)
+    GRC_EDM_FLUKT_ERR          = GRC_EDM + 8 # Too much turbulences or distractions
+    GRC_EDM_FMOT_ERR           = GRC_EDM + 9 # Filter motor defective
+    
+    # Subsystem errors 
+    GRC_EDM_DEV_NOT_INSTALLED  = GRC_EDM + 10 # Device like EGL, DL is not installed.
+    GRC_EDM_NOT_FOUND          = GRC_EDM + 11 # Search result invalid. For the exact explanation \nsee in the description of the called function.
+    GRC_EDM_ERROR_RECEIVED     = GRC_EDM + 12 # Communication ok, but an error\nreported from the EDM sensor.
+    GRC_EDM_MISSING_SRVPWD     = GRC_EDM + 13 # No service password is set.
+    GRC_EDM_INVALID_ANSWER     = GRC_EDM + 14 # Communication ok, but an unexpected\nanswer received. 
+    GRC_EDM_SEND_ERR           = GRC_EDM + 15 # Data send error, sending buffer is full.
+    GRC_EDM_RECEIVE_ERR        = GRC_EDM + 16 # Data receive error, like\nparity buffer overflow.
+    GRC_EDM_INTERNAL_ERR       = GRC_EDM + 17 # Internal EDM subsystem error.
+    GRC_EDM_BUSY               = GRC_EDM + 18 # Sensor is working already,\nabort current measuring first.
+    GRC_EDM_NO_MEASACTIVITY    = GRC_EDM + 19 # No measurement activity started.
+    GRC_EDM_CHKSUM_ERR         = GRC_EDM + 20 # Calculated checksum, resp. received data wrong\n(only in binary communication mode possible).
+    GRC_EDM_INIT_OR_STOP_ERR   = GRC_EDM + 21 # During start up or shut down phase an\nerror occured. It is saved in the DEL buffer.
+    GRC_EDM_SRL_NOT_AVAILABLE  = GRC_EDM + 22 # Red laser not available on this sensor HW.
+    GRC_EDM_MEAS_ABORTED       = GRC_EDM + 23 # Measurement will be aborted (will be used for the lasersecurity)
+    
+    # New TPS1200 sensor user error
+    GRC_EDM_SLDR_TRANSFER_PENDING = GRC_EDM + 30 # Multiple OpenTransfer calls.
+    GRC_EDM_SLDR_TRANSFER_ILLEGAL = GRC_EDM + 31 # No opentransfer happened.
+    GRC_EDM_SLDR_DATA_ERROR       = GRC_EDM + 32 # Unexpected data format received.
+    GRC_EDM_SLDR_CHK_SUM_ERROR    = GRC_EDM + 33 # Checksum error in transmitted data.
+    GRC_EDM_SLDR_ADDR_ERROR       = GRC_EDM + 34 # Address out of valid range.
+    GRC_EDM_SLDR_INV_LOADFILE     = GRC_EDM + 35 # Firmware file has invalid format.
+    GRC_EDM_SLDR_UNSUPPORTED      = GRC_EDM + 36 # Current (loaded) firmware doesn't support upload.
+    GRC_EDM_UNKNOW_ERR	          = GRC_EDM + 40 # Undocumented error from the\nEDM sensor, should not occur.
+    
+    GRC_EDM_DISTRANGE_ERR         = GRC_EDM + 50 # Out of distance range (dist too small or large)
+    GRC_EDM_SIGNTONOISE_ERR       = GRC_EDM + 51 # Signal to noise ratio too small
+    GRC_EDM_NOISEHIGH_ERR         = GRC_EDM + 52 # Noise to high
+    GRC_EDM_PWD_NOTSET            = GRC_EDM + 53 # Password is not set
+    GRC_EDM_ACTION_NO_MORE_VALID  = GRC_EDM + 54 # Elapsed time between prepare und start fast measurement for ATR to long
+    GRC_EDM_MULTRG_ERR            = GRC_EDM + 55 # Possibly more than one target (also a sensor error)
+    
+    GRC_MOT_UNREADY     = GRC_MOT + 0 #motorization is not ready (1792)
+    GRC_MOT_BUSY        = GRC_MOT + 1 #motorization is handling another task (1793)
+    GRC_MOT_NOT_OCONST  = GRC_MOT + 2 #motorization is not in velocity mode (1794)
+    GRC_MOT_NOT_CONFIG  = GRC_MOT + 3 #motorization is in the wrong mode or busy (1795)
+    GRC_MOT_NOT_POSIT   = GRC_MOT + 4 #motorization is not in posit mode (1796)
+    GRC_MOT_NOT_SERVICE = GRC_MOT + 5 #motorization is not in service mode (1797)
+    GRC_MOT_NOT_BUSY    = GRC_MOT + 6 #motorization is handling no task (1798)
+    GRC_MOT_NOT_LOCK    = GRC_MOT + 7 #motorization is not in tracking mode (1799)
+    GRC_MOT_NOT_SPIRAL  = GRC_MOT + 8 #motorization is not in spiral mode (1800)
+    
+    GRC_TMC_NO_FULL_CORRECTION = GRC_TMC + 3      # Warning: measurment without full correction
+    GRC_TMC_ACCURACY_GUARANTEE = GRC_TMC + 4      # Info   : accuracy can not be guarantee
+    
+    GRC_TMC_ANGLE_OK              = GRC_TMC + 5   # Warning: only angle measurement valid
+    GRC_TMC_ANGLE_NOT_FULL_CORR   = GRC_TMC + 8   # Warning: only angle measurement valid but without full correction
+    GRC_TMC_ANGLE_NO_ACC_GUARANTY = GRC_TMC + 9   # Info   : only angle measurement valid but accuracy can not be guarantee
+    
+    GRC_TMC_ANGLE_ERROR       = GRC_TMC + 10      # Error  : no angle measurement
+    
+    GRC_TMC_DIST_PPM          = GRC_TMC + 11      # Error  : wrong setting of PPM or MM on EDM
+    GRC_TMC_DIST_ERROR        = GRC_TMC + 12      # Error  : distance measurement not done (no aim, etc.)
+    GRC_TMC_BUSY              = GRC_TMC + 13      # Error  : system is busy (no measurement done)
+    GRC_TMC_SIGNAL_ERROR      = GRC_TMC + 14      # Error  : no signal on EDM (only in signal mode)
+    
+    GRC_BMM_XFER_PENDING           = GRC_BMM +  1 # Loading process already opened
+    GRC_BMM_NO_XFER_OPEN           = GRC_BMM +  2 # Transfer not opened
+    GRC_BMM_UNKNOWN_CHARSET        = GRC_BMM +  3 # Unknown character set
+    GRC_BMM_NOT_INSTALLED          = GRC_BMM +  4 # Display module not present
+    GRC_BMM_ALREADY_EXIST          = GRC_BMM +  5 # Character set already exists
+    GRC_BMM_CANT_DELETE            = GRC_BMM +  6 # Character set cannot be deleted
+    GRC_BMM_MEM_ERROR              = GRC_BMM +  7 # Memory cannot be allocated
+    GRC_BMM_CHARSET_USED           = GRC_BMM +  8 # Character set still used
+    GRC_BMM_CHARSET_SAVED          = GRC_BMM +  9 # Charset cannot be deleted or is protected
+    GRC_BMM_INVALID_ADR            = GRC_BMM + 10 # Attempt to copy a character block\noutside the allocated memory
+    GRC_BMM_CANCELANDADR_ERROR     = GRC_BMM + 11 # Error during release of allocated memory
+    GRC_BMM_INVALID_SIZE           = GRC_BMM + 12 # Number of bytes specified in header\ndoes not match the bytes read
+    GRC_BMM_CANCELANDINVSIZE_ERROR = GRC_BMM + 13 # Allocated memory could not be released
+    GRC_BMM_ALL_GROUP_OCC          = GRC_BMM + 14 # Max. number of character sets already loaded
+    GRC_BMM_CANT_DEL_LAYERS        = GRC_BMM + 15 # Layer cannot be deleted
+    GRC_BMM_UNKNOWN_LAYER          = GRC_BMM + 16 # Required layer does not exist
+    GRC_BMM_INVALID_LAYERLEN       = GRC_BMM + 17 # Layer length exceeds maximum
+
+
+class byte(int):
+    def __new__(cls, value, *args, **kwargs):
+        if (type(value) == str):
+            value = int(value.strip("'"), 16)
+        elif (type(value) == bytes):
+            value = int(value.strip(b"'"), 16)
+        
+        if value < 0:
+            raise ValueError("byte types must not be less than zero")
+        if value > 255:
+            raise ValueError("byte types must not be more than 255 (0xff)")
+        
+        return  super(cls, cls).__new__(cls, value)
+
+class PrismType(Enum):
+    ROUND        = 0  # prism type: round
+    MINI         = 1  # prism type: mini
+    TAPE         = 2  # prism type: tape
+    DEG360       = 3  # prism type: 360
+    USER1        = 4  # prism type: user1 
+    USER2        = 5  # prism type: user2
+    USER3        = 6  # prism type: user3
+    DEG360_MINI  = 7  # prism type: 360 mini 
+    MINI_ZERO    = 8  # prism type: mini zero 
+    USER         = 9  # prism type: user
+    HDS_TAPE     = 10 # prism type: tape cyra
+    GRZ121_ROUND = 11 # prism type: GRZ121 round for machine guidance
+
 class TargetType(Enum):
     REFLECTOR = 0
     REFLECTORLESS = 1
@@ -22,7 +252,7 @@ class TMCMeasurementMode(Enum):
     TMC_RED_TRK_DIST = 10   # Red laser tracking
     TMC_FREQUENCY = 11      # Frequency measurement (test)
 
-class EdmMode(Enum):
+class EDMMode(Enum):
     EDM_MODE_NOT_USED = 0   # Init value
     EDM_SINGLE_TAPE = 1     # Single measurement with tape
     EDM_SINGLE_STANDARD = 2 # Standard single measurement
@@ -95,26 +325,39 @@ class RecordFormat(Enum):
     GSI_8  = 0
     GSI_16 = 1
 
-class TpsStatus(Enum):
-    OFF = 0
+class TPSStatus(Enum):
+    OFF      = 0
     SLEEPING = 1
-    ONLINE = 2
-    LOCAL = 3
-    UNKNOWN = 4
+    ONLINE   = 2
+    LOCAL    = 3
+    UNKNOWN  = 4
 
-class byte(int):
-    def __new__(cls, value, *args, **kwargs):
-        if (type(value) == str):
-            value = int(value.strip("'"), 16)
-        elif (type(value) == bytes):
-            value = int(value.strip(b"'"), 16)
-        
-        if value < 0:
-            raise ValueError("byte types must not be less than zero")
-        if value > 255:
-            raise ValueError("byte types must not be more than 255 (0xff)")
-        
-        return  super(cls, cls).__new__(cls, value)
+class OnOff(Enum):
+    OFF = 0
+    ON = 1
+
+class EGLIntensity(Enum):
+    OFF  = 0
+    LOW  = 1
+    MID  = 2
+    HIGH = 3
+
+class ControllerMode(Enum):
+    RELATIVE_POSITIONING = 0
+    CONSTANT_SPEED       = 1
+    MANUAL_POSITIONING   = 2
+    LOCK_IN              = 3
+    BRAKE                = 4
+    TERMINATE            = 7
+
+class ControllerStopMode(Enum):
+    NORMAL   = 0
+    SHUTDOWN = 1
+    
+class LockInStatus(Enum):
+    LOCKED_OUT = 0
+    LOCKED_IN  = 1
+    PREDICTION = 2
 
 class PyGeoCom:
     def __init__(self, stream):
@@ -148,9 +391,11 @@ class PyGeoCom:
 
         parameters = parameters.rstrip()
         rpc_return_code, *parameters = parameters.split(b',')
-        rpc_return_code = int(rpc_return_code)
-        assert rpc_return_code == 0
+        rpc_return_code = ReturnCode(int(rpc_return_code))
         
+        if (rpc_return_code != ReturnCode.GRC_OK):
+            raise Exception(rpc_return_code)
+
         return parameters
 
     def get_instrument_number(self) -> int:
@@ -222,3 +467,35 @@ class PyGeoCom:
         if number_of_digits > 15:
             raise ValueError("Number of digits must be lesser than or equal to 15")
         self.__request(107, (number_of_digits,))
+    
+    def laser_pointer(self, state: OnOff):
+        self.__request(1004, (state.value,))
+
+    def laser_pointer_on(self):
+        self.laser_pointer(OnOff.ON)
+
+    def laser_pointer_off(self):
+        self.laser_pointer(OnOff.OFF)
+
+    # Not tested as I don't have a device with an EGL
+    def get_egl_intensity(self) -> EGLIntensity:
+        intensity, = self.__request(1058)
+        return EGLIntensity(int(intensity))
+
+    # Not tested as I don't have a device with an EGL
+    def set_egl_intensity(self, intensity: EGLIntensity):
+        self.__request(1059, (intensity.value,))
+
+    def get_motor_lock_status(self) -> LockInStatus:
+        motor_lock_status, = self.__request(6021)
+        return LockInStatus(int(motor_lock_status))
+
+    def start_controller(self, controller_mode: ControllerMode):
+        self.__request(6001, (controller_mode.value,))
+
+    def stop_controller(self, controller_stop_mode: ControllerStopMode):
+        self.__request(6002, (controller_stop_mode.value,))
+
+    # Speed is in radians/second, with a maximum of ±0.79rad/s each
+    def set_velocity(self, hoziontal_speed: float, vertical_speed: float):
+        self.__request(6004, (hoziontal_speed, vertical_speed))
